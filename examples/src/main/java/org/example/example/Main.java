c6a38e6b5d0d3d19131215adfb0154574940db2d
student <student@localhost>
2024-03-12 11:02:33 +0300

    initial commit

---
+++ examples/src/main/java/org/example/example/Main.java
@@ -0,0 +1,9 @@
+package org.example.example;
+
+import org.example.server.Server;
+
+public class Main {
+    public static void main(String[] args) {
+        Server server = new Server();
+    }
+}
